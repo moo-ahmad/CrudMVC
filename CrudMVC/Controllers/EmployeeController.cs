ee55feb64ff13aac045f12d4c4ebefd81690c557
Muhammmad Ahmad <muhammadahmad399@gmail.com>
2021-11-18 18:11:54 +0500

    sa

--- CrudMVC/Controllers/EmployeeController.cs
+++ CrudMVC/Controllers/EmployeeController.cs
@@ -7,6 +7,9 @@ using System.Web;
 using System.Web.Mvc;
 using CrudMVC.Models;
 using System.Diagnostics;
+using PagedList.Mvc;
+using PagedList;
+
 namespace CrudMVC.Controllers
 {
     public class EmployeeController : Controller
@@ -16,21 +19,33 @@ namespace CrudMVC.Controllers
         EmployeeEntities2 dbobj2 = new EmployeeEntities2();
 
         [HttpGet]
-        public ActionResult Employee()
+        public ActionResult Employee(int? page, string sortOrder,string searchString)
         {
-
+            int pageSize = 10;
+            int pageIndex = 1;
+            pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             var res = dbobj2.employees.ToList();
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    res = res.OrderByDescending(s => s.FullName).ToList();
+                    break;
+                
+                default:
+                    res = res.OrderBy(s => s.FullName).ToList();
+                    break;
+            }
+            
+            IPagedList<employee> emp = res.ToPagedList(pageIndex, pageSize);
 
-            return View(res);
+            return View(emp);
         }
+      
 
         [HttpPost]
         public ActionResult AddEmployee(employee model)
         {
-            Debug.Write("We are in.....");
-
-
-
             try
             {
 
@@ -52,6 +67,8 @@ namespace CrudMVC.Controllers
 
             return View("Employee", res);
         }
-
+        
+       
+        
     }
 }
